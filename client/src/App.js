5f9c3093a89763ba74efdd2188065ca225bdc2b2
n01586869 <n01586869@humber.ca>
2024-02-18 17:51:28 -0500

    added generic fetchData function

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,36 @@
 import './App.css';
 // import { useState, useEffect } from 'react'
 import Experience from './Experience'
+import Education from './Education';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
   const API = "http://localhost:8000"
+
+  // generic fetchData function to be passed to all children
+  const fetchData = (endpoint, setData) => {
+    {fetch(`${API}${endpoint}`)
+        .then((res) => {
+        if (res.status !== 200) {
+            console.log("error");
+            return Promise.reject("Exit promise");
+        }
+        // console.log(res)
+        return res.json();
+        })
+        .then((json) => {
+        setData(json);
+        // console.log(json);
+        })
+        .catch((err) => console.log("Err: " + err))}
+   }
     
 
   return(
     <div style={{backgroundColor: '#F9F2F1'}}>
-      <Experience API={API}/>
+      <Education API={API} fetchData={fetchData}/>
+      <Experience API={API} fetchData={fetchData} />
     </div>
     );
   }
