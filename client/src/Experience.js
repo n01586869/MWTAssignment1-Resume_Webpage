6792f3fd90b5ab8db2ea8c324a6aa942e2070986
n01586869 <n01586869@humber.ca>
2024-02-18 17:52:17 -0500

    implemented fetchData

--- client/src/Experience.js
+++ client/src/Experience.js
@@ -1,53 +1,39 @@
 import { useEffect, useState } from "react";
-import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Experience({ API }) {
+export default function Experience({ fetchData }) {
 
     const [data, setData] = useState()
 
-    useEffect(()=>
-    {fetch(`${API}/getExp`)
-        .then((res) => {
-        if (res.status !== 200) {
-            console.log("error");
-            return Promise.reject("Exit promise");
-        }
-        // console.log(res)
-        return res.json();
-        })
-        .then((json) => {
-        setData(json);
-        // console.log(json);
-        })
-        .catch((err) => console.log(err))}
+    useEffect(() =>
+        fetchData('/getExp', setData)
     )
 
     return (
-    <div>
-        {data && data.map((exp)=>{
-            return (
-                <div className="mx-auto" style={{maxWidth: 800, paddingTop: 15, paddingBottom: 15}}>
-                        <div style={{fontSize: 18}}>{exp.company}</div>
-                        <div style={{display: "flex", justifyContent: 'space-between', color: '#006DA3'}}>
+        <div className="mx-auto" style={{ maxWidth: 800, paddingTop: 15, paddingBottom: 15 }}>
+            <div style={{fontSize: 24, color: '#4A556D'}}>Experience</div>
+            <hr />
+            {data && data.map((exp) => {
+                return (
+                    <div>
+                        <div style={{ fontSize: 18 }}>{exp.company}</div>
+                        <div style={{ display: "flex", justifyContent: 'space-between', color: '#006DA3' }}>
                             <div>
-                            {exp.jobTitle}
+                                {exp.jobTitle}
                             </div>
-                            <div style={{display: 'flex'}}>
+                            <div style={{ display: 'flex' }}>
                                 <div>{exp.startDate} -&nbsp;</div>
                                 <div> {exp.endDate}</div>
                             </div>
                         </div>
                         <div>
-                            {exp.responsibilities.map((resp)=>{
-                            return <div>&#x2022; {resp}</div>
-                        })}
+                            {exp.responsibilities.map((resp) => {
+                                return <div>&#x2022; {resp}</div>
+                            })}
                         </div>
-                </div>
-            )
-        })}
-    </div>
+                    </div>
+                )
+            })}
+        </div>
     );
 }
-
-export default Experience;
