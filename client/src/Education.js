dfbfda1d30b89e48db5f7c6800358aa29c894bb3
n01586869 <n01586869@humber.ca>
2024-02-18 20:59:42 -0500

    Made Education into Card

--- client/src/Education.js
+++ client/src/Education.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { Card } from 'react-bootstrap'
 
 export default function Education({ fetchData }) {
 
@@ -9,36 +10,43 @@ export default function Education({ fetchData }) {
     )
 
     return (
-        <div className="mx-auto component">
-            <div className="header">Education</div>
-            <hr />
-            {/* While data is true, loop through each object in Edu array */}
-            {data && data.map((edu) => {
-                return (
-                    <div key={edu.school}>
-                        {/* Display school name */}
-                        <div className="subheader">{edu.school}</div>
+        <>
+        {data &&
+            <Card className="mx-auto component">
+                <Card.Header>
+                    <div className="header">Education</div>
+                </Card.Header>
+                <Card.Body>
+                    {/* While data is true, loop through each object in Edu array */}
+                    {data.map((edu) => {
+                        return (
+                            <div key={edu.school}>
+                                {/* Display school name */}
+                                <div className="subheader">{edu.school}</div>
 
-                        <div style={{ fontSize: 14 }}>
-                            {/* Display school city and province */}
-                            {edu.city} -&nbsp;
-                            {edu.province}
-                        </div>
-                        {/* Container for degree type and program, and start/graduation date */}
-                        <div style={{ display: "flex", justifyContent: 'space-between', color: '#006DA3' }}>
-                            <div>
-                                {/* Display degree type and program name */}
-                                {edu.type} in {edu.program}
+                                <div style={{ fontSize: 14 }}>
+                                    {/* Display school city and province */}
+                                    {edu.city} -&nbsp;
+                                    {edu.province}
+                                </div>
+                                {/* Container for degree type and program, and start/graduation date */}
+                                <div style={{ display: "flex", justifyContent: 'space-between', color: '#006DA3' }}>
+                                    <div>
+                                        {/* Display degree type and program name */}
+                                        {edu.type} in {edu.program}
+                                    </div>
+                                    <div style={{ display: 'flex' }}>
+                                        {/* Display start date and graduation date */}
+                                        <div>{edu.startDate} -&nbsp;</div>
+                                        <div> {edu.graduationDate}</div>
+                                    </div>
+                                </div>
                             </div>
-                            <div style={{ display: 'flex' }}>
-                                {/* Display start date and graduation date */}
-                                <div>{edu.startDate} -&nbsp;</div>
-                                <div> {edu.graduationDate}</div>
-                            </div>
-                        </div>
-                    </div>
-                )
-            })}
-        </div>
+                        )
+                    })}
+                </Card.Body>
+            </Card>
+        }
+        </>
     )
 }
