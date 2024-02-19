e5a4ee87b2c8675caa746d52b7d4289d78346b9b
n01586869 <n01586869@humber.ca>
2024-02-18 20:52:56 -0500

    Made Skills into Card

--- client/src/Skills.js
+++ client/src/Skills.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { Card } from 'react-bootstrap'
 
 export default function Skills({ fetchData }) {
 
@@ -9,18 +10,26 @@ export default function Skills({ fetchData }) {
     })
 
     return (
-        <div className="mx-auto component">
-            <div className="header">Skills</div>
-            <hr />
-            {/* While data is true, loop through each skill in array */}
-            {data && data.map((skill) => {
-                return (
-                    <div key={skill}>
-                        {/* Displays a bullet point, then the current skill */}
-                        &#x2022; {skill}
-                    </div>
-                )
-            })}
-        </div>
+        <>
+        {data && 
+            <Card className="mx-auto component">
+                <Card.Header className="header">
+                    <div className="header">Skills</div>
+                </Card.Header>
+                <Card.Body>
+                    {/* While data is true, loop through each skill in array */}
+                    {data && data.map((skill) => {
+                        return (
+                            <div key={skill}>
+                                {/* Displays a bullet point, then the current skill */}
+                                &#x2022; {skill}
+                            </div>
+                        )
+                    })}
+                </Card.Body>
+            </Card>
+        }
+        </>
+
     )
 }
